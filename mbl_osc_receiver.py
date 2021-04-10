1400ec972430641d08483268290b9a8bebff584e
Michele <michele.romani.gzl0@me.com>
2021-04-10 11:59:03 +0200

    :sparkles: update template for osc receiver

--- mbl_osc_receiver.py
+++ mbl_osc_receiver.py
@@ -1,10 +1,14 @@
-import osc4py3
+from osc4py3.as_eventloop import *
+from osc4py3 import oscbuildparse
+from osc4py3 import oscmethod as osm
+from osc4py3 import oscchannel as osch
 
 class MBLOscReceiver:
 
 
     def __init__(self, port=9001):
         #init 
+        self.oscReceiver
         self.initialize()
 
     def initialize(self):
@@ -18,5 +22,58 @@ class MBLOscReceiver:
         self.power1 = 0
         self.power2 = 0
 
-    def update():
+    def update(self):
+        #foo
+
+
+    # Getters
+    def get_scene(self):
+        return self.scene
+
+    def get_time(self):
+        return self.time    
+
+    def get_progress(self):
+        return self.progress
+
+    def get_numHeadSets(self):
+        return self.numHeadSets
+
+    def get_correlation(self):
+        return self.correlation
+
+    def get_score(self):
+        return self.score
+
+    def get_power1(self):
+        return self.power1
+
+    def get_power2(self):
+        return self.power2
+
+    # Setters
+    def set_scene(self, scene):
+        self.scene = scene
+
+    def set_time(self, time):
+        self.time = time
+
+    def set_progress(self, progress):
+        self.progress = progress
+
+    def set_numHeadSets(self, numHeadSets):
+        self.numHeadSets = numHeadSets
+
+    def set_correlation(self, correlation):
+        self.correlation = correlation
+
+    def set_score(self, score):
+        self.score = score
+
+    def set_power1(self, power1):
+        self.power1 = power1
+
+    def set_power2(self, power2):
+        self.power2 = power2
+
         
