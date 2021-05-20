cb63db02ec9343621542a62e161ce6bd4c9df833
David Medine <david.eric.medine@gmail.com>
2021-05-20 16:36:45 +1000

    adding example LSL receiver

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ from osc4py3.as_eventloop import *
 from osc4py3 import oscbuildparse
 from osc4py3 import oscmethod as osm
 from osc4py3 import oscchannel as osch
+import time
+#from mbl_osc_receiver import MBLOscReceiver
 
-from mbl_osc_receiver import MBLOscReceiver
+#osc_receiver = MBLOscReceiver("Rvalues")
+#osc_receiver.initialize()
 
-osc_receiver = MBLOscReceiver("Rvalues")
-osc_receiver.initialize()
+from mbl_lsl_receiver import MBLLSLReceiver
+
+lsl_receiver = MBLLSLReceiver()
+lsl_receiver.resolve_streams()
+lsl_receiver.start_listener()
+time.sleep(10)
+lsl_receiver.stop_listener()
