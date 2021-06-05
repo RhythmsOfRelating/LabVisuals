193f1a243269c03d878526d3e707015355eafaba
BRomans <michele.romani.gzl0@gmail.com>
2021-06-05 12:19:22 +0200

    :sparkles: implemented threading to receive data with gui loaded.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 import time
+import threading
 from PyQt5.QtWidgets import QApplication, QDialog
 from visuals.mbl_gui_window import MBL_GuiWindow
 from lsl.mbl_lsl_receiver import MBL_LSLReceiver
 
 
 def receive_data_threaded(receiver):
+    print("Thread started")
     receiver.resolve_streams()
     receiver.start_listener()
     time.sleep(10)
@@ -15,9 +17,13 @@ def receive_data_threaded(receiver):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     gui_window = MBL_GuiWindow()
-    gui_window.show()
     lsl_receiver = MBL_LSLReceiver()
+    gui_window.show()
+
+    receiver_thread = threading.Thread(target=receive_data_threaded(lsl_receiver))
+    receiver_thread.start()
 
     sys.exit(app.exec_())
+
 
 
