aef2fbf0b857bf82a12a76fc7bf7001f6b6c2f1a
BRomans <michele.romani.gzl0@gmail.com>
2021-06-05 11:51:18 +0200

    :recycle: folder structure refactored for better clarity.

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from osc4py3.as_eventloop import *
-from osc4py3 import oscbuildparse
-from osc4py3 import oscmethod as osm
-from osc4py3 import oscchannel as osch
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog
 import time
-#from mbl_osc_receiver import MBLOscReceiver
+# from mbl_osc_receiver import MBLOscReceiver
 
-#osc_receiver = MBLOscReceiver("Rvalues")
-#osc_receiver.initialize()
+# osc_receiver = MBLOscReceiver("Rvalues")
+# osc_receiver.initialize()
+from gui import Ui_MainWindow
+from lsl.mbl_lsl_receiver import MBLLSLReceiver
 
-from mbl_lsl_receiver import MBLLSLReceiver
 
-lsl_receiver = MBLLSLReceiver()
-lsl_receiver.resolve_streams()
-lsl_receiver.start_listener()
-time.sleep(10)
-lsl_receiver.stop_listener()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    Dialog = QDialog()
+    ui = Ui_MainWindow()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    lsl_receiver = MBLLSLReceiver()
+    lsl_receiver.resolve_streams()
+    lsl_receiver.start_listener()
+    time.sleep(10)
+    # lsl_receiver.stop_listener()
+    sys.exit(app.exec_())
